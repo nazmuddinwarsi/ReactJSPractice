6b75821fc32e41853735a008499f5632f058c411
nazmuddinwarsi <nazmuddinwarsi@gmail.com>
2024-11-02 12:09:48 +0530

    Class Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
+class App extends React.Component 
+{
+  render(){
+    return(
+      <h1>Nazmuddn Warsi</h1>
+    )
+  }
+
+}
 // function App() {
 //   return (
 //     <div className="App">
@@ -23,23 +33,23 @@ import './App.css';
 //     </div>
 //   );
 // }
-const   App=()=> <div className="App">
-    <div className="new"></div>
+// const   App=()=> <div className="App">
+//     <div className="new"></div>
 
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learninng for  ever
-        </a>
-      </header>
-    </div>
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload by Warsi.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learninng for  ever
+//         </a>
+//       </header>
+//     </div>
 
 export default App;
